743684f5d11711ff30bbffa89eb934423e31fbb4
Nageen Chand <25462010+archie9211@users.noreply.github.com>
2019-01-18 23:31:20 +0530

    Update PostInstall.sh

--- PostInstall.sh
+++ PostInstall.sh
@@ -3,44 +3,46 @@ if [ -f /etc/debian_version ]; then
 	echo "Debian/Ubuntu"
 	sudo apt update
 	sudo apt upgrade    
-	sudo apt install docky
-	sudo apt install gedit
-	sudo apt install openvpn
-	sudo apt install git
-	sudo apt install vim 
+	sudo apt install git vim 
+	#lets install chrome 
+	rm -rf /etc/apt/sources.list.d/*chrome*
+	rm -rf /etc/apt/*chrome*
+	
 	wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 	echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 	sudo apt-get update && sudo apt-get install google-chrome-stable
 	sudo apt install zsh
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	mkdir "$HOME/.vim"
+	curl -o "$HOME/.vim/vimrc" "https://raw.githubusercontent.com/archie9211/scripts/8fcfba08135823635112748dee14bb262214768b/vimrc"
+	git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+        git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+
+
 
 elif [ -f /etc/arch-release ]; then
 	echo "Arch Linux"
 	sudo pacman -Syu reflector 
-	sudo reflector --verbose --latest 10 --sort rate --save /etc/pacman.d/mirrorlist 
+	sudo reflector --country India --verbose --latest 10 --sort rate --save /etc/pacman.d/mirrorlist 
 	sudo pacman -Sy vim vlc tmux openssh gnome-tweaks julia git cmake clang
-	sudo systemctl enable sshd.service
+#	sudo systemctl enable sshd.service
 	git clone https://aur.archlinux.org/yay.git
-	cd yay
+	cd yay || exit
 	makepkg -si
-	cd -
+	cd - || exit
 	yay -S zsh google-chrome sublime-text-dev  
 	yay -S ttf-google-fonts-opinionated-git
 	yay -S bibata-cursor-theme
 	yay -S grub2-theme-archlinux
 	yay -S vertex-themes 
-	
+	sudo pacman -S zsh
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-	
-	
-# elif [ -f /etc/redhat-release ] || [ -f /etc/system-release-cpe ]; then
-# 	echo "Red Hat / CentOS"
-# elif [ -f /etc/SUSE-brand ] || [ -f /etc/SuSE-brand ] || [ -f /etc/SuSE-release ] || [ -d /etc/susehelp.d ]; then
-# 	echo "SuSE"
-# elif [ "$OPERATING_SYSTEM_TYPE" = "FreeBSD" ]; then
-# 	echo "FreeBSD"
-# elif [ "$OPERATING_SYSTEM_TYPE" = "OpenBSD" ]; then
-# 	echo "OpenBSD"
-# elif [ "$OPERATING_SYSTEM_TYPE" = "Cygwin" ]; then
-# 	echo "Cygwin"
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	mkdir "$HOME/.vim"
+	curl -o "$HOME/.vim/vimrc" "https://raw.githubusercontent.com/archie9211/scripts/8fcfba08135823635112748dee14bb262214768b/vimrc"
+	git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+        git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+
 fi
